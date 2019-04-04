33509bcf32e44e616ebe5f57404e577e18f71474
phil <uniphil@gmail.com>
2019-04-04 19:56:23 -0400

    some sketchy plans

--- k103.ino
+++ k103.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 #define BOLEX_SWITCH  12 // pulls down
 #define BOLEX_SHUTTER 8  // active low
 #define K103_ADVANCE  4  // active high "drive"
@@ -5,6 +7,64 @@
 #define K103_TAKEUP   9  // active high???
 #define K103_FWD_SW   2  // pulls down
 #define K103_REV_SW   3  // pulls down
+
+/**
+ * K-103 control system
+ * rewired for AC isolation and a bit of automation by phil for madi, 2019
+ * 
+ * the main aluminum control box has responsibilities:
+ * - all hardware IO
+ *    -> exposed as basic functions, so that we can do
+ * - frame counting (for both k103 and bolex)
+ *    -> should be persisted to eeprom
+ * - a nice command-oriented serial interface
+ * 
+ * the main control box is specifically not responsible for
+ * - automation
+ * - a directly-usable interface
+ * 
+ * command set:
+ * 
+ * Reels
+ * - load film (ts, desc, len, curr frame)
+ * 
+ * Frames
+ * - bolex: capture frame
+ * - k103: advance reel
+ * - k103: reverse reel
+ * 
+ * EEPROM shape
+ * 
+ * Reel: 20 bytes
+ * off  size  desc            type
+ *   0  4     load timestamp  unsigned long (NB: year 2106)
+ * + 4  8     description     char[8]
+ * +12  4     reel length     long
+ * +16  4     current frame   long
+ * 
+ */
+
+#define EEP_BOLEX_OFFSET   0
+#define EEP_K103_OFFSET   40
+// 20-byte extra gap: can add features if needed without having to realign
+// or even, add a second camera?
+
+struct Reel {
+  unsigned long ts;
+  char desc[8];
+  long len;
+  long frame;
+};
+
+Reel bolex;
+Reel k103;
+
+void restore_reel_state(Reel * r, uint16_t eep_offset) {
+  EEPROM.get(eep_offset + 0, r->ts);
+  EEPROM.get(eep_offset + 4, r->desc);
+  EEPROM.get(eep_offset + 12, r->len);
+  EEPROM.get(eep_offset + 16, r->frame);
+}
 
 void setup() {
   pinMode(BOLEX_SWITCH, INPUT_PULLUP);
@@ -18,63 +78,28 @@ void setup() {
   pinMode(K103_FWD_SW, INPUT_PULLUP);
   pinMode(K103_REV_SW, INPUT_PULLUP);
 
-//  attachInterrupt(digitalPinToInterrupt(K103_FWD_SW), fwd_int, FALLING);
-//  attachInterrupt(digitalPinToInterrupt(K103_REV_SW), rev_int, FALLING);
+  attachInterrupt(digitalPinToInterrupt(K103_FWD_SW), fwd_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(K103_REV_SW), rev_isr, FALLING);
+
+  restore_reel_state(&bolex, EEP_BOLEX_OFFSET);
+  restore_reel_state(&k103, EEP_K103_OFFSET);
 
   Serial.begin(9600);
   Serial.println("hello");
 }
 
-void fwd_int() {
-  Serial.print(millis());
-  Serial.println(" fwd int");
+void fwd_isr() {
+}
+void rev_isr() {
 }
 
-void rev_int() {
-  Serial.print(millis());
-  Serial.println(" rev int");
+void handle_serial() {
+  if (Serial.available() > 0) {
+    
+  }
 }
 
 void loop() {
-
-  analogWrite(K103_TAKEUP, 82);
-  digitalWrite(K103_ADVANCE, HIGH);
-  delay(30);
-  digitalWrite(K103_ADVANCE, LOW);
-  delay(1000);
-  analogWrite(K103_TAKEUP, 0);
-
-  delay(1500);
-  digitalWrite(K103_REVERSE, LOW);
-  delay(1500);
-
-  analogWrite(K103_TAKEUP, 82);
-  digitalWrite(K103_ADVANCE, HIGH);
-  delay(30);
-  digitalWrite(K103_ADVANCE, LOW);
-  delay(1000);
-  analogWrite(K103_TAKEUP, 0);
-  delay(1000);
-
-  digitalWrite(K103_REVERSE, HIGH);
-  while(1);
-
-//  digitalWrite(K103_TAKEUP, LOW);
-//  delay(1000);
-//
-//  Serial.print(millis());
-//  Serial.println(" triggering advance");
-//  digitalWrite(K103_ADVANCE, HIGH);
-//  delay(30);
-//  digitalWrite(K103_ADVANCE, LOW);
-//
-////  unsigned long t = pulseIn(BOLEX_SWITCH, LOW);
-////  Serial.print("pulsed atfter ");
-////  Serial.println(t);
-//
-//  delay(1000);
-//  Serial.print(millis());
-//  Serial.println(" bye");
-//  while (1);
+  handle_serial();
 }
 
